10c5250667a8f13d5e8bbb240e1a39b9a75bf4f8
alexapatricia <alexandra.pillajo.est@tecazuay.edu.ec>
2022-04-06 23:15:00 -0500

    mi primer ejercicio commit

---
+++ src/main/java/ista/sistema02/sistema02/Sistema02Application.java
@@ -0,0 +1,13 @@
+package ista.sistema02.sistema02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sistema02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sistema02Application.class, args);
+	}
+
+}
---
+++ src/test/java/ista/sistema02/sistema02/Sistema02ApplicationTests.java
@@ -0,0 +1,13 @@
+package ista.sistema02.sistema02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sistema02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
